1b76807f19753c0dd7ec67dbc0419a98fc0d3f8d
Chris Jao <jao.chris@gmail.com>
2017-12-20 06:53:29 -0700

    Var and Str

---
+++ javascript/learn-to-code.js
@@ -0,0 +1,21 @@
+//NOTES:
+//variable(var) is a like a bucket
+//and in that bucket has something in it 
+//and that something is a "strings"
+
+
+//Sample
+//var name = "Chris";
+//var age = 29;
+//
+//var message = "Hi, My Name is " + name + " and I am " + age + " Years old";
+
+var firstName = "John";
+var lastName = "something";
+var dateOfBirth = "10-09-82";
+var age = 23;
+var profileImgUrl = "https://media0.giphy.com/media/S2S0ZDytY6yDm/giphy.gif"
+
+var loginWelcomeMessage = "Welcome, " + firstName + ". Happy " + age + "rd birthday!"; 
+
+console.log(loginWelcomeMessage);
